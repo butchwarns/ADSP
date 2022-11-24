8b4ec6b070f57554bf08b37db9d1d10f0b615667
Butch Warns <contact@butchwarns.de>
2022-11-24 14:51:04 +0100

    Add first-order all-pass filters

---
+++ source/filter/ApHp1.cpp
@@ -0,0 +1,101 @@
+/*
+  ==============================================================================
+    ApHp1.cpp
+
+    Copyright (C) 2022 Butch Warns
+    All rights reserved.
+
+    contact@butchwarns.de
+
+    BSD 2-Clause License
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+        list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ==============================================================================
+*/
+
+#include "ApHp1.h"
+
+namespace adsp {
+ApHp1::ApHp1() {}
+ApHp1::~ApHp1() {}
+
+void ApHp1::reset(double sampleRate) {
+    sampleRate = sampleRate;
+
+    // Setup biquad object
+
+    // Default implementation
+    BiquadParams biquadParams = biquad.getParameters();
+    biquadParams.calculationType = biquadAlgorithm::transposedCanonical;
+    biquad.setParameters(biquadParams);
+
+    // Clear biquad state array
+    biquad.reset();
+}
+
+double ApHp1::process(double x) { return biquad.process(x); }
+
+//==============================================================================
+
+void ApHp1::setSampleRate(double sampleRate) {
+    sampleRate = sampleRate;
+
+    // Calculate new coefficients
+    calculateFilterCoefficients();
+}
+
+ApHp1Params ApHp1::getParameters() { return params; }
+
+void ApHp1::setParameters(const ApHp1Params &parameters) {
+    // If new parameters differ..
+    if (params.fc != parameters.fc) {
+        // Update the parameters
+        params = parameters;
+
+        // Calculate coefficients with new parameters
+        calculateFilterCoefficients();
+    } else {
+        // Otherwise do nothing
+        return;
+    }
+}
+
+void ApHp1::calculateFilterCoefficients() {
+    // Clear coefficient array
+    memset(&coefficientsArray[0], 0, sizeof(double) * numCoefficients);
+
+    // Calculate coefficients
+    const double w0 = 2.0 * PI * params.fc;
+
+    const double a = 1.0 / sampleRate * w0;
+    const double a0b1 = (a - 2.0) / (a + 2.0);
+
+    // Update coefficient array
+    coefficientsArray[a0] = -a0b1;
+    coefficientsArray[a1] = -1;
+    coefficientsArray[a2] = 0.0;
+    coefficientsArray[b1] = a0b1;
+    coefficientsArray[b2] = 0.0;
+
+    biquad.setCoefficients(coefficientsArray);
+}
+}  // namespace adsp
---
+++ source/filter/ApLp1.h
@@ -0,0 +1,144 @@
+/*
+  ==============================================================================
+    ApLp1.h
+
+    Copyright (C) 2022 Butch Warns
+    All rights reserved.
+
+    contact@butchwarns.de
+
+    BSD 2-Clause License
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+        list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ==============================================================================
+*/
+
+/**
+* @file ApLp1.h
+*
+* @brief All-pass filter based on the RC low-pass, first-order
+*/
+
+#pragma once
+
+#include "Biquad.h"
+
+namespace adsp {
+/**
+* @brief ApLp1 parameter structure
+* 
+*/
+struct ApLp1Params {
+    ApLp1Params(){};
+
+    ApLp1Params &operator=(const ApLp1Params &parameters) {
+        if (this == &parameters) {
+            return *this;
+        } else {
+            fc = parameters.fc;
+            return *this;
+        }
+    }
+
+    // Center frequency
+    double fc = 4000.0;  // Hz
+};
+
+/**
+* @brief All-pass filter based on the RC low-pass, first-order 
+* 
+* Analog modeled by means of a prewarped bilinear transformation.  
+* Prewarping was chosen to match center frequencies.  
+* This filter has not been decramped.  
+*/
+class ApLp1 {
+   public:
+    ApLp1();
+    ~ApLp1();
+
+    //==============================================================================
+
+    /**
+    * @brief Reset to default parameters, clear internal state and set sample rate
+    * 
+    * @param sampleRate New sample rate
+    */
+    void reset(double sampleRate);
+
+    /**
+    * @brief Proess a single sample
+    * 
+    * @param x Input sample
+    * @return Output sample 
+    */
+    double process(double x);
+
+    //==============================================================================
+
+    /**
+    * @brief Set sample rate, recalculate coefficients
+    * 
+    * @param sampleRate 
+    */
+    void setSampleRate(double sampleRate);
+
+    /**
+    * @brief Get parameters
+    * 
+    * @return Filter parameters 
+    */
+    ApLp1Params getParameters();
+
+    /**
+    * @brief Set parameters
+    * 
+    * @param New filter parameters
+    */
+    void setParameters(const ApLp1Params &parameters);
+
+   protected:
+    /**
+    * @brief Sample rate
+    */
+    double sampleRate{48000.0};
+
+    /**
+    * @brief  Object implementing the difference equation
+    */
+    Biquad biquad;
+
+    /**
+    * @brief Filter coefficients
+    */
+    double coefficientsArray[numCoefficients] = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    /**
+    * @brief Filter parameters
+    */
+    ApLp1Params params;
+
+    /**
+    * @brief Recalculate coefficients, is called when filter parameters change
+    */
+    void calculateFilterCoefficients();
+};
+}  // namespace adsp
